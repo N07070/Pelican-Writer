0e8c41e8ca640768dae07d36cfe6e52272bd07ec
N07070 <nelson.perdriau@gmail.com>
2016-02-07 16:37:19 +0100

    Finished the main logic

--- main.py
+++ main.py
@@ -3,9 +3,19 @@
 
 """
 
-article_title = input("Please enter your title : ")
-article_creator = input("Please enter your name : ")
-article_tags = input("Please input your tags : ")
-article_summary = input("Please enter your summary : ")
-article_catergory = intput("Please input your category : ")
-article_header = input("Please enter the name of your image : ")
+import datetime
+
+article_title = str(input("Please enter your title : "))
+article_creation_date = str(datetime.datetime.now())
+article_category = str(input("Please input your category : "))
+article_tags = str(input("Please input your tags : "))
+artice_slug = str(article_title.lower().replace(" ","-").encode('ascii',errors='ignore'))
+article_creator = str(input("Please enter your name : "))
+article_summary = str(input("Please enter your summary : "))
+article_header = str(input("Please enter the name of your image : "))
+
+final = "Title: " + article_title + "\nDate: " + article_creation_date + "\nCategory: " + article_category + "\nTags: " + article_tags + "\nSlug: " + artice_slug + "\nAuthors: "+article_creator+"\nSummary: "+article_summary + "\n\n "
+
+article = open(artice_slug + ".md", "w")
+article.write(final)
+article.close()

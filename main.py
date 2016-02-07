61c3929962d720f7cb7015dab8a4cb9026372c62
N07070 <nelson.perdriau@gmail.com>
2016-02-07 13:33:52 +0100

    Added main file

---
+++ main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+
+"""
+
+article_title = input("Please enter your title : ")
+article_creator = input("Please enter your name : ")
+article_tags = input("Please input your tags : ")
+article_summary = input("Please enter your summary : ")
+article_catergory = intput("Please input your category : ")
+article_header = input("Please enter the name of your image : ")

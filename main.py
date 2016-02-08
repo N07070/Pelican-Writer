43f199b73ed3157ed59a198352d6303bb1f68b04
N07070 <nelson.perdriau@gmail.com>
2016-02-08 20:54:08 +0100

    Finished.

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ article_title = str(input("Please enter your title : "))
 article_creation_date = str(datetime.datetime.now())
 article_category = str(input("Please input your category : "))
 article_tags = str(input("Please input your tags : "))
-artice_slug = str(article_title.lower().replace(" ","-").encode('ascii',errors='ignore'))
+artice_slug = str(article_title.lower().replace(" ","-").encode('ascii',errors='ignore')).replace("b'","").replace("'","")
 article_creator = str(input("Please enter your name : "))
 article_summary = str(input("Please enter your summary : "))
 article_header = str(input("Please enter the name of your image : "))
 
-final = "Title: " + article_title + "\nDate: " + article_creation_date + "\nCategory: " + article_category + "\nTags: " + article_tags + "\nSlug: " + artice_slug + "\nAuthors: "+article_creator+"\nSummary: "+article_summary + "\n\n "
+final = "Title: " + article_title + "  \nDate: " + article_creation_date + "  \nCategory: " + article_category + "  \nTags: " + article_tags + "  \nSlug: " + artice_slug + "  \nAuthors: "+article_creator+"  \nSummary: "+article_summary + "  \n\n "
 
 article = open(artice_slug + ".md", "w")
 article.write(final)
---
+++ this-is-an-example,-a-simple-one-!.md
@@ -0,0 +1,9 @@
+Title: This is an example, a simple one !  
+Date: 2016-02-08 20:53:10.088229  
+Category: example  
+Tags: example,guide,  
+Slug: this-is-an-example,-a-simple-one-!  
+Authors: N07070  
+Summary: A brief example  
+
+ 
